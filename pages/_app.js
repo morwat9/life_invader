c92cdf6fa558d70613a0fd6e2db933cdfbd52261
Michael Orwat <morwat9@hotmail.com>
2023-01-14 00:27:51 -0800

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FCA311",
+      contrastText: "#FFFFFF"
+    },
+    secondary: {
+      main: "#14213D"
+    }
+  }
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
